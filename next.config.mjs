bf8900a8cc10e67b80ddf8db95bb8c8cae16460d
yousuf-4594 <cysiddiqui@gmail.com>
2024-10-22 01:21:10 +0500

    commit message

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
   }
-  
-  module.exports = nextConfig
+
+export default nextConfig;
