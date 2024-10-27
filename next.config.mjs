765485b722c043c6734ae8e89408b2e584d1b5a0
yousuf-4594 <cysiddiqui@gmail.com>
2024-10-28 01:45:20 +0500

    commit message

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone',
-    experimental: {
-      outputFileTracingRoot: undefined,
-      appDir: true
-    },
-    // Disable tracing to prevent the trace file issue
-    outputFileTracing: false,
-    // Add this to change build directory if needed
-    distDir: 'build'
-  }
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
