5d7fc0065a7c9343b1559c09d937c7b58f6cd2a1
yousuf-4594 <cysiddiqui@gmail.com>
2024-11-10 18:53:21 +0500

    main

---
+++ src/app/server.js
@@ -0,0 +1,20 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 8080;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(`> Ready on port ${port}`);
+  });
+});
+
+
