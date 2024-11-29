da108c505cc81650d02ee3b42f6ae14a9ae38706
yousuf-4594 <cysiddiqui@gmail.com>
2024-11-29 12:57:18 +0500

    commit message

--- src/utils/NextAuthOptions.ts
+++ src/utils/NextAuthOptions.ts
@@ -41,7 +41,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET!,
   callbacks: {
     session: async ({ session, token }) => {
       console.log("Session User ID:", token.sub);
